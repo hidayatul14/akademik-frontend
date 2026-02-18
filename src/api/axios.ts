b2e2b5a85488c618252a1eb9c1ba38f0f51b1089
hidayatul14 <gantengdayat81@gmail.com>
2026-02-18 15:30:21 +0700

    fiks axios

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "https://akademik-api-production.up.railway.app/api",
 });
 
 export default api;
